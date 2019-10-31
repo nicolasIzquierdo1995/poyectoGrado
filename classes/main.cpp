9a23c8f40b3bd187d52c52d8497a0f15ff7f3855
gonlarghero <gonlarghero@gmail.com>
2019-10-31 19:15:13 -0300

    -se agregan namespaces a variables main

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include "inputOutput.hpp"
- 
-using namespace InputOutput;
+
 using namespace std;
 int main (int argc, char *argv[])
 {
-  Arguments args = InputOutput.ProcessArguments(argc, argv);
+  InputOutput::Arguments args = InputOutput.ProcessArguments(argc, argv);
   Runner executor = new runner();
   if(!args.IsOK){
     cout<<"Argumentos erroneos"<<endl;
     cout<<"El formato correcto es: f5grado filePath compressionLevel multiThreading"<<endl;
     return 0;
   }
-  Results res =  runner.processFile(args.File,args.compressionLevel);
+  Runner::Results res =  runner.processFile(args.File,args.compressionLevel);
   cout<<"Compression finished"<<endl;
   cout<<"Resulted in " + res.comp + "% compressions"<<endl;
   return 0;
