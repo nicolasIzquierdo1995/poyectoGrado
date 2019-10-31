a694e53e5ad8a2f6f5e60d7dc347ceecce9c568e
gonlarghero <gonlarghero@gmail.com>
2019-10-31 19:05:27 -0300

    -agrega flujo al main

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include "inputOutput.hpp"
  
-int main(int argc, char *argv[])
+using namespace InputOutput;
+using namespace std;
+int main (int argc, char *argv[])
 {
-  std::cout << "Hello World2!" << std::endl;
+  Arguments args = InputOutput.ProcessArguments(argc, argv);
+  Runner executor = new runner();
+  if(!args.IsOK){
+    cout<<"Argumentos erroneos"<<endl;
+    cout<<"El formato correcto es: f5grado filePath compressionLevel multiThreading"<<endl;
+    return 0;
+  }
+  Results res =  runner.processFile(args.File,args.compressionLevel);
+  cout<<"Compression finished"<<endl;
+  cout<<"Resulted in " + res.comp + "% compressions"<<endl;
   return 0;
 }
